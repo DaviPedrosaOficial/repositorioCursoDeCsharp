8baa7c14e11e4b6be4f399b3627736ca154bcc29
DaviPedrosa <pedrosassss@hotmail.com>
2024-02-21 09:25:11 -0300

    Enviando o primeiro exercício

---
+++ Exercício_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO.Pipes;
+
+namespace primeiraAula
+{
+    class Exercício_1
+    {
+        static void Main(string[] arg)
+        {
+            string curso = "ciências da computação";
+            string nome = "Davi";
+            int idade = 24;
+            string uni = "Una";
+            
+            Console.WriteLine("Olá mundo, esse é o meu primeiro programa em C#!");
+            Console.WriteLine("Meu nome é {0}, tenho {1} anos e no momento estou cursando em {2} pela universidade {3}!",nome,idade,curso,uni);
+            Console.WriteLine("\r\nO intuito desse primeiro programa é aprender a utilizar o comando 'Console.WriteLine' e suas formatações.");
+        }
+    }
+    
+}
