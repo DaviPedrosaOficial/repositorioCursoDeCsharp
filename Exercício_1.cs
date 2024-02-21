f66bcef8efb86d7ce8cb1e21bbc6bb8f2e278345
DaviPedrosa <pedrosassss@hotmail.com>
2024-02-21 10:44:22 -0300

    Completando primeiro exercício Foi se inserido mais um padrão de formatação, além dos comentários explicativos

--- Exercício_1.cs
+++ Exercício_1.cs
@@ -11,10 +11,21 @@ namespace primeiraAula
             string nome = "Davi";
             int idade = 24;
             string uni = "Una";
+            double numero = 12.3568;
             
+            /* Comando de saida em texto (WriteLine salta linha ao final da frase, Write não salta linhas). */
             Console.WriteLine("Olá mundo, esse é o meu primeiro programa em C#!");
+            
+            /* Utilizando {}'s para inserir variavéis dentro do texto de saída. */
             Console.WriteLine("Meu nome é {0}, tenho {1} anos e no momento estou cursando em {2} pela universidade {3}!",nome,idade,curso,uni);
+            
+            /* Utilizando o comando '\r\n' para saltar linhas onde desejado. */
             Console.WriteLine("\r\nO intuito desse primeiro programa é aprender a utilizar o comando 'Console.WriteLine' e suas formatações.");
+            
+            /* Utilizando o padrão de formatação para numeros decimais (Para isso utilizamos o comando '.ToString("F2")' onde o 'F'
+             representa a quantidade de casas decimais desejadas para o numero, ou seja, como nesse caso gostariamos de alocar apenas
+             2 casas decimais, foi-se utilizado F2. */
+            Console.WriteLine("\r\nUtilizando a formatação de numeros decimais -> {0} ",numero.ToString("F2"));
         }
     }
     
