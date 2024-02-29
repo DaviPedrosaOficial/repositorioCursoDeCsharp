17a988196fd66b2f02034acc0a9709c0d745dc4d
DaviPedrosa <pedrosassss@hotmail.com>
2024-02-29 11:09:10 -0300

    Enviando primeiros exercicios da ficha de propostos 2

--- ExercicioPropostos_2.cs
+++ ExercicioPropostos_2.cs
@@ -9,6 +9,50 @@ namespace CursoDeCsharp
         static void Main(string[] arg)
         {
             /* ------ 1º Exercício ------ */
+            /* Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não. */
+
+            /* Entrada */
+            Console.WriteLine("------ Exercicio 1 ------");
+            Console.WriteLine("Insira a seguir um numero inteiro de sua escolha, e diremos se ele e positivo ou negativo: ");
+            int numPosNeg = int.Parse(Console.ReadLine());
+            
+            /* Processamento & saída */
+            if (numPosNeg >= 0)
+            {
+                Console.WriteLine("O numero {0} e positivo!",numPosNeg);
+            }
+            else
+            {
+                Console.WriteLine("O numero {0} e negativo!",numPosNeg);
+            }
+            
+            
+            
+            /* ------- Exercício 2 ------- */
+            /* Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar. */
+            
+            /* Entrada */
+            Console.WriteLine("\r\n------ Exercicio 2 ------");
+            Console.WriteLine("Insira a seguir um numero inteiro de sua escolha, e diremos se ele e par ou impar: ");
+            int numParImp = int.Parse(Console.ReadLine());
+            
+            /* Processamento & saída */
+
+            if (numParImp % 2 == 0)
+            {
+                Console.WriteLine("O numero {0} e par!", numParImp);
+            }
+            else
+            {
+                Console.WriteLine("O numero {0} e impar!",numParImp);
+            }
+            
+            
+            
+            /* ------ Exercício 3 ------ */
+            /* Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+            Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser 
+            digitados em ordem crescente ou decrescente. */
         }
     }
 }
