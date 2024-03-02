fb036d90674a062bf8c87bc455bd2c2cd63e3051
DaviPedrosa <pedrosassss@hotmail.com>
2024-03-02 13:26:28 -0300

    Encerrada a lista de exercícios propostos 2

--- ExercicioPropostos_2.cs
+++ ExercicioPropostos_2.cs
@@ -325,6 +325,72 @@ namespace CursoDeCsharp
             {
                 Console.WriteLine("O ponto se localiza no quadrante 'Q1'!");
             }
+            
+            
+            
+            /* ------ Exercício 8 ------ */
+            /* Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos,
+             pois sabem que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício
+             da população, sem qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$. 
+ 
+             Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. 
+             Em seguida, calcule e mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo. 
+ 
+             Renda                                Imposto de Renda
+             de 0 a 2000,00                            Isento
+             de 2000,01 a 3000,00                   8% de imposto
+             de 3000,01 a 4500,00                  18% de imposto
+             de 4500,01 acima                      28% de imposto
+ 
+             Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de
+             salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. 
+             No exemplo fornecido (abaixo), a taxa é de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em 
+             R$ 80.36 no total. O valor deve ser impresso com duas casas decimais.     */
+
+            /* Entrada */
+            Console.WriteLine("------ Exercicio 8 ------");
+            Console.WriteLine("Bem vindo ao calculador de imposto de renda.\r\nInsira a seguir o valor de seu salario para que possamos calcular o valor que voce devera pagar:");
+            double salCliente = double.Parse(Console.ReadLine());
+
+            double impRenda = 0;
+            
+            /* Processamento e saída */
+
+            if (salCliente < 0)
+            {
+                Console.WriteLine("\r\nValor inserido para o salario e invalido!");
+            }
+            else if (salCliente > 4500)
+            {
+                /* Como o imposto é acima de R$4500,00, os valores de impostos ate os R$4500,00 são fixos, respectivamente
+                 R$79,99 para 8% de imposto, e R$269,99 para 18% de imposto. */
+
+                impRenda = ((salCliente - 4500) * 0.28) + 79.9992 + 269.9982;
+
+                Console.WriteLine("\r\nSeu imposto de renda sera R${0:F2} !",impRenda);
+            }
+            else if (salCliente > 3000)
+            {
+                /* Como o imposto é acima de R$3000,00, o valor de imposto ate os R$3000,00 é fixo, sendo ele R$79,99 para
+                 8% de imposto. */
+
+                impRenda = ((salCliente - 3000) * 0.18) + 79.9992;
+
+                Console.WriteLine("\r\nSeu imposto de renda sera R${0:F2} !",impRenda);
+            }
+            else if (salCliente > 2000)
+            {
+                /* Como o imposto so será cobrado acima de R$2000,00, deveremos retirar R$2000,00 para calcularmos o
+                 imposto */
+                
+                impRenda = (salCliente - 2000) * 0.08;
+                
+                Console.WriteLine("\r\nSeu imposto de renda sera R${0:F2} !",impRenda);
+            }
+            else
+            {
+                Console.WriteLine("\r\nVoce esta isento do imposto de renda!");
+            }
         }
     }
 }
--- test.cs
+++ test.cs
@@ -6,32 +6,7 @@ namespace CursoDeCsharp
     {
         static void Main(string[] arg)
         {
-            Console.WriteLine("\r\n------ Exercicio 6 ------");
-            Console.WriteLine("Neste programas verificaremos qual intervalo entre 0 e 100, se localiza um numero de\r\nsua escolha!");
-            Console.WriteLine("Entao para que possamos comeca-lo, insira a seguir um numero de sua escolha: ");
-            int numCliente = int.Parse(Console.ReadLine());
-
-            /* Processamento e saída */
-            if (numCliente < 0 || numCliente > 100)
-            {
-                Console.WriteLine("\r\nValor fora do intervalo 0 - 100.");   
-            }
-            else if (numCliente >= 0 && numCliente <= 25)
-            {
-                Console.WriteLine("\r\nValor se encontra no intervalo entre 0 e 25!");
-            }
-            else if (numCliente > 25 && numCliente <= 50)
-            {
-                Console.WriteLine("\r\nValor se encontra no intervalo entre 25 e 50!");
-            }
-            else if (numCliente > 50 && numCliente <= 75)
-            {
-                Console.WriteLine("\r\nValor se encontra no intervalo entre 50 e 75!");
-            }
-            else
-            {
-                Console.WriteLine("\r\nValor se encontra no intervalo entre 75 e 100!");
-            }
+            /* Hub de teste */
         }
     }
 }
