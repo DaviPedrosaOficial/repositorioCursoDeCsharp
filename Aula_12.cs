97f5bd940489621e5c9733f3d64443286cc6fc3a
DaviPedrosa <pedrosassss@hotmail.com>
2024-03-05 09:15:37 -0300

    Iniciando aula 12

---
+++ Aula_12.cs
@@ -0,0 +1,52 @@
+﻿/* ------ Estrutura repetitiva enquanto (while) ------ */
+
+using System;
+
+namespace CursoDeCsharp
+{
+    class Aula_12
+    {
+        static void _Aula12(string[] arg)
+        {
+            /* O que é a estrutura while?
+             
+              A estrutura while nada mais é do que um loop onde o mesmo será executado enquanto sua condicional for verdade
+             e assim que a mesma se tornar falsa o mesmo se encerrará!
+              É interessante salientarmos que sua estrutura é bem semelhante a das estruturas condicionais, tendo como 
+             parâmetro uma condicional que determinará se deverá ou não ser executado, e os comandos dentro de seu escopo
+             aguardando para serem executados caso o loop seja ativo.
+             
+             Estrutura do comando:
+             
+             while (condicional) {
+                   comando 1
+                   comando 2
+                }
+                  
+             Lembre-se: o loop while, testa uma condição, e se verdadeira ele irá executar os comandos de seu escopo, e 
+             assim em que esses se encerrarem, ele voltará a testar a condicional, e se a mesma continuar sendo verdadeira
+             ele executará novamente seus comandos até que sua condicional se torne falsa.
+             
+             Agora vamos demonstrá-lo na prática:   */
+
+            /* Entrada */
+            Console.WriteLine("Digite um numero para que possamos lhe voltar sua raiz quadrada: ");
+            double numCliente = double.Parse(Console.ReadLine());
+            
+            /* Processamento & saída (loop while) */
+            while (numCliente >= 0)
+            {
+                double raizCliente = Math.Sqrt(numCliente);
+                Console.WriteLine("A raiz quadrada de {0:F2} é {1:F2} !",numCliente,raizCliente);
+                Console.WriteLine("\r\nInsira outro numero para calcularmos sua raiz quadrada: ");
+                numCliente = double.Parse(Console.ReadLine());
+
+            }
+            
+            /* Saída (fora do loop) */
+            Console.WriteLine("O numero inserido é negativo!");
+            
+        }
+    } 
+}
+
--- test.cs
+++ test.cs
@@ -7,65 +7,18 @@ namespace CursoDeCsharp
         static void Main(string[] arg)
         {
             /* Hub de teste */
-            
-            Console.WriteLine("Insira a seguir tres numeros inteiros, para que possamos averiguar qual deles e o maior");
-            Console.WriteLine("Insira o primeiro numero: ");
-            int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Insira o segundo numero: ");
-            int n2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Insira o terceiro e ultimo numero: ");
-            int n3 = int.Parse(Console.ReadLine());
 
-            /* Ao invés de escrevermos o código abaixo, todas as vezes que formos fazer tal comparação em nosso programa,
-             delegamos esse trabalho a uma função. A mesma se encontra no final do código,após o fechamento da função de 
-             execução do programa, uma vez que o próprio static void _Aula11(string [] arg) é também uma função!
-             
-             if (n1 > n2 && n1 > n3)
+            Console.WriteLine("Digite um numero para que possamos lhe voltar sua raiz quadrada: ");
+            double numCliente = double.Parse(Console.ReadLine());
+            while (numCliente >= 0)
             {
-                Console.WriteLine("\r\nO numero {0} e o maior entre os tres escolhidos!",n1);
-            }
-            else if (n2 > n3)
-            {
-                Console.WriteLine("\r\nO numero {0} e o maior entre os tres escolhidos!",n2);
-            }
-            else
-            {
-                Console.WriteLine("\r\nO numero {0} e o maior entre os tres escolhidos!",n3);
-            }  */
-            
-            /* ------ Após criada a função basta chamarmos a mesma ------ */
-            Console.WriteLine("O numero {0} e o maior entre os tres escohidos!",Maior(n1,n2,n3));
+                double raizCliente = Math.Sqrt(numCliente);
+                Console.WriteLine("A raiz quadrada de {0:F2} é {1:F2} !",numCliente,raizCliente);
+                Console.WriteLine("\r\nInsira outro numero para calcularmos sua raiz quadrada: ");
+                numCliente = double.Parse(Console.ReadLine());
 
-        }
-        
-        /* Nomenclatura para a criação de nossa função:
-         
-         Ao criarmos uma função devemos, por enquanto inserir static a frente (pois a mesma entrara em uma outra static),
-         depois inserir o tipo que retornará nessa função (nesse nosso caso será 'int', uma vez que ela verificará somente
-         numeros inteiros), depois inserimos o nome da mesma e seus argumentos ou parâmentros dentro do parênteses.
-         
-         Após sua criação, inserimos dentro da mesma os comandos que desejamos para a mesma, e qual sera seu retorno, se 
-         desejado.
-         
-         Veja a seguir como fica:  */
-
-        static int Maior(int a, int b, int c)
-        {
-            int maior = 0;
-            if (a > b && a > b)
-            {
-                maior = a;
             }
-            else if (b > c)
-            {
-                maior = b;
-            }
-            else
-            {
-                maior = c;
-            }
-
-            return maior;
+            Console.WriteLine("O numero inserido é negativo!");
         }
     }
 }
