6b587ab4f2a21a64b982daa9f8f1b91a5d8bf1c9
DaviPedrosa <pedrosassss@hotmail.com>
2024-03-05 12:08:39 -0300

    Atualizando lista de exercícios

--- ExercicioPropostos_3.cs
+++ ExercicioPropostos_3.cs
@@ -111,6 +111,29 @@ namespace CursoDeCsharp
                     y = double.Parse(vetor[1]);
                 }
             }
+            
+            
+            
+            
+            /* ------ Exercício 3 ------ */
+            /* Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes.
+             Escreva um algoritmo para ler o tipo de combustível abastecido da seguinte forma: 
+             (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel4.Fim). 
+             Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código 
+             (até que seja válido). O programa será encerrado quando o código informado for o número 4. 
+             Deve ser escrito a mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de 
+             combustível, conforme o exemplo abaixo.
+             
+             Entrada                                         Saída
+                8                                        Muito Obrigado!
+                1                                           Álcool: 1
+                7                                           Gasolina: 2
+                2                                           Diesel: 0
+                2
+                4                                                                */
+
+            Console.WriteLine("\r\n ------ Exercicio 3 ------");
+            Console.WriteLine();
         }
     }
 }
--- test.cs
+++ test.cs
@@ -8,75 +8,7 @@ namespace CursoDeCsharp
         {
             /* Hub de teste */
 
-            Console.WriteLine("\r\n ------ Exercicio 2 ------");
-            Console.WriteLine("Bem vindo ao Quadrant Finder, nesse programa iremos receber as coordenadas x e y, e retornaremos");
-            Console.WriteLine("qual quadrante as mesmas fazem parte! ");
-
-            double x = 0;
-            double y = 0;
             
-            /* Entrada */
-            Console.WriteLine("Para iniciarmos, insira o valor de x e y a seguir (na mesma linha): ");
-            string[] vetor = Console.ReadLine().Split(' ');
-
-            x = double.Parse(vetor[0]);
-            y = double.Parse(vetor[1]);
-
-            /* Processamento */
-            while ((x > 0 && y > 0) || (x < 0 && y < 0) || (x > 0 && y < 0) || (x < 0 && y > 0))
-            {
-                /* Quadrante 1 (x e y positivos) */
-                if (x > 0 && y > 0)
-                {
-                    /* Saída */
-                    Console.WriteLine("\r\nO vetor descrito se encontra no quadrante Q1 !");
-                    /* Entrada */
-                    Console.Write("Insira outro vetor para que possamos lhe dizer onde o mesmo se localiza: ");
-                    
-                    vetor = Console.ReadLine().Split(' ');
-                    x = double.Parse(vetor[0]);
-                    y = double.Parse(vetor[1]);
-                }
-                
-                /* Quadrante 2 (x negativo e y positivo) */
-                else if (x < 0 && y > 0)
-                {
-                    /* Saída */
-                    Console.WriteLine("\r\nO vetor descrito se encontra no quadrante Q2 !");
-                    /* Entrada */
-                    Console.Write("Insira outro vetor para que possamos lhe dizer onde o mesmo se localiza: ");
-                    
-                    vetor = Console.ReadLine().Split(' ');
-                    x = double.Parse(vetor[0]);
-                    y = double.Parse(vetor[1]);
-                }
-                
-                /* Quadrante 3 (x e y negativos)*/
-                else if (x < 0 && y < 0)
-                {
-                    /* Saída */
-                    Console.WriteLine("\r\nO vetor descrito se encontra no quadrante Q3 !");
-                    /* Entrada */
-                    Console.Write("Insira outro vetor para que possamos lhe dizer onde o mesmo se localiza: ");
-
-                    vetor = Console.ReadLine().Split(' ');
-                    x = double.Parse(vetor[0]);
-                    y = double.Parse(vetor[1]);
-                }
-
-                /* Quadrante 4 (x positivo e y negativo) */
-                else
-                {
-                    /* Saída */
-                    Console.WriteLine("\r\nO vetor descrito se encontra no quadrante Q4 !");
-                    /* Entrada */
-                    Console.Write("Insira outro vetor para que possamos lhe dizer onde o mesmo se localiza: ");
-
-                    vetor = Console.ReadLine().Split(' ');
-                    x = double.Parse(vetor[0]);
-                    y = double.Parse(vetor[1]);
-                }
-            }
         }
     }
 }
