f3fa18f8cc3e404795db46754c04c71c68a5bf10
DaviPedrosa <pedrosassss@hotmail.com>
2024-03-08 10:57:00 -0300

    Iniciando o segundo cap do curso

--- Cap2_ClassesAtributosMetodosMembrosEstaticos/Aula_1.cs
+++ Cap2_ClassesAtributosMetodosMembrosEstaticos/Aula_1.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿/* ------ Resolvendo um problema sem orientação a objeto ------ */
+
+using System;
 
 namespace CursoDeCsharp
 {
-    class test
+    class Aula_1
     {
-        static void Main(string[] arg)
+        static void _Aula1(string[] arg)
         {
-            /* Hub de teste */
+            /* A ideia por trás desse exercício é mostrar a diferença entre desenvolvermos soluções para problemas
+             a partir da orientação à objetos. Sendo assim, iremos desenvolver a solução para um problema sem a sua 
+             utilização e depois, demonstraremos a diferença na prática, trabalhando o conceito! */
+            
+            /* Exemplo a ser desenvolvido abaixo */
+            
+            /* Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas válidas).
+             Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos possui a maior área.
+             A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
+             seguinte (fórmula de Heron): 
+             
+             area = Raiz quadrada de -> p( p - a)( p - b)( p - c)
+             
+             onde p = (a + b + c) / 2                                                                           */
 
             Console.WriteLine("Bem vindo ao comparador de triangulos!");
             Console.WriteLine("Nesse programa, calcularemos a area de dois triangulos baseados nas medidas de cada lado");
@@ -43,8 +58,8 @@ namespace CursoDeCsharp
             double area2 = Math.Sqrt(p2 * (p2 - a_2) * (p2 - b_2) * (p2 - c_2));
             
             /* Saída */
-            Console.WriteLine("\r\nArea do 1º triangulo: {0:F2}",area1);
-            Console.WriteLine("Area do 2º triangulo: {0:F2}",area2);
+            Console.WriteLine("\r\nArea do 1º triangulo: {0:F4}",area1);
+            Console.WriteLine("Area do 2º triangulo: {0:F4}",area2);
 
             if (area1 == area2)
             {
@@ -58,8 +73,8 @@ namespace CursoDeCsharp
             {
                 Console.WriteLine("O 2º triangulo é maior que o 1º triangulo!");
             }
+            
+
         }
     }
 }
-
-
