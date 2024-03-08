909bc683c53bc52678e83cc80f632e4b9f16c31b
DaviPedrosa <pedrosassss@hotmail.com>
2024-03-08 18:27:29 -0300

    Enviando atualizações

--- Cap2_ClassesAtributosMetodosMembrosEstaticos/Aula2.cs
+++ Cap2_ClassesAtributosMetodosMembrosEstaticos/Aula2.cs
@@ -22,7 +22,7 @@ namespace CursoDeCsharp
             
             /* ------ Classes ------ */
             /* É um tipo estruturado que pode conter membros, sendo esses:
-             - Atributos: (dados / campos)
+             - Atributos: (dados / campos) (no caso do triangulo seriam as medidas dos lados 'a', 'b' e 'c')
              - Métodos: (funções / operações)                             */
             
             /* A classe pode prover muitos outros recursos, tais como:
@@ -32,8 +32,78 @@ namespace CursoDeCsharp
              - Herança
              - Polimorfismo                                               */
             
-            /* Exemplos:
-             - Entidades: Produto, Cliente,  */
+            /* Exemplos (de possíveis classes):
+             - Entidades: Produto, Cliente, Triangulo
+             - Serviços: ProdutoService, ClienteService, EmailService, StorageService
+             - Controladores: ProdutoController, ClienteController
+             - Utilitários: Calculadora, Compactador
+             - Outros: (views, repositórios, gerenciadores, etc) */
+            
+            /* Agora, que entendemos o conceito por trás das classes, vamos começar a utilizá-las, começando pela classe
+             triangulo.
+             Então para prosseguir nos estudos, basta mudar para o arquivo Triangulo.cs para acompanhar o desenvolvimento 
+             da classe!*/
+            
+            /* Refazendo o exemplo a partir da utilização da orientação à objetos:      */
+            
+            Console.WriteLine("Bem vindo ao comparador de triangulos!");
+            Console.WriteLine("Nesse programa, calcularemos a area de dois triangulos baseados nas medidas de cada lado");
+            Console.WriteLine("que serao fornecidas por voces!");
+   
+            /* Declarando as variáveis */
+            Triangulo x, y;
+ 
+            /* Instânciando as variáveis */
+            x = new Triangulo();
+            y = new Triangulo();
+            
+            /* 1º Triangulo */
+            Console.WriteLine("\r\n------ 1º Triangulo ------");
+            Console.Write("Entao, para que possamos prosseguir, insira a seguir a medida do lado 'a' do 1º triangulo:  ");
+            /* Para acessarmos atríbutos de nossos objetos basta colocar o nome do objeto, seguido do ponto "." e o nome
+             do atríbuto, como na execução abaixo: */
+            x.A = double.Parse(Console.ReadLine());
+
+            Console.Write("Agora, insira a medida do lado 'b' do 1º triangulo: ");
+            x.B = double.Parse(Console.ReadLine());
+            
+            Console.Write("Agora, insira a medida do lado 'c' do 1º triangulo: ");
+            x.C = double.Parse(Console.ReadLine());
+            
+            /* 2º Triangulo */
+            Console.Write("\r\n------ 2º Triangulo ------");
+            Console.Write("Agora, insira a medida do lado 'a' do 2º triangulo: ");
+            y.A = double.Parse(Console.ReadLine());
+
+            Console.Write("Agora, insira a medida do lado 'b' do 2º triangulo: ");
+            y.B = double.Parse(Console.ReadLine());
+
+            Console.Write("E por ultimo, insira a medida do lado 'c' do 2º triangulo: ");
+            y.C = double.Parse(Console.ReadLine());
+            
+            /* Processamento */
+            double p1 = (x.A + x.B + x.C) / 2;
+            double p2 = (y.A + y.B + y.C) / 2;
+
+            double area1 = Math.Sqrt(p1 * (p1 - x.A) * (p1 - x.B) * (p1 - x.C));
+            double area2 = Math.Sqrt(p2 * (p2 - y.A) * (p2 - y.B) * (p2 - y.C));
+            
+            /* Saída */
+            Console.WriteLine("\r\nArea do 1º triangulo: {0:F4}",area1);
+            Console.WriteLine("Area do 2º triangulo: {0:F4}",area2);
+
+            if (area1 == area2)
+            {
+             Console.WriteLine("Os triangulos tem o mesmo tamanho!");
+            }
+            else if (area1 > area2)
+            {
+             Console.WriteLine("O 1º triangulo é maior que o 2º triangulo!");
+            }
+            else
+            {
+             Console.WriteLine("O 2º triangulo é maior que o 1º triangulo!");
+            }
         }
     }
 } 
---
+++ Cap2_ClassesAtributosMetodosMembrosEstaticos/Triangulo.cs
@@ -0,0 +1,9 @@
+﻿namespace CursoDeCsharp;
+
+public class Triangulo
+{
+    /* Atributos da classe (lembrado que nomes de atributos, começam com letra maiúscula)*/
+    public double A;
+    public double B;
+    public double C;
+}
