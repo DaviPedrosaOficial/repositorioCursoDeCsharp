508c249996e20ec299242d0a35e631f9ef8df620
DaviPedrosa <pedrosassss@hotmail.com>
2024-03-13 11:11:01 -0300

    Enviando inicio da aula 1 do 3 capitulo

---
+++ Cap_3_Construtores&PalavraThis&Sobrecarga&Encapsulamento/Aulas/Aula1_3Cap.cs
@@ -0,0 +1,26 @@
+﻿/* ------ Construtores ------ */
+
+namespace CursoDeCsharp
+{
+    class Aula1_3Cap{
+        
+        static void _Aula1_3Cap(string[] arg)
+        {
+            /* O que são contrutores?
+             
+             Construtores são operações especiais de uma classe, onde são executados logo em seu instanciamento!
+             
+             Ou seja, ao criar um objeto de uma classe que possui contrutores, teremos de declarar logo em sua instanciação
+             o que o objeto terá para cada um dos mesmos.
+             
+             Pensando em um caso hipotético, se uma classe Funcionário, possuísse o um atríbuto .Nome em seu construtor,
+             sempre que fossemos adicionar um novo "Funcionário" (objeto) a ela, teriamos que inserir em sua instanciação 
+             o nome daquele funcionario!
+             
+             ------ Usos Comuns dos Construtores ------
+             * Iniciar valores dos atributos de uma classe
+             * Permitir ou obrigar que o objeto receba dados / depêndencias no momento de sua instanciação (injeção de
+             depêndencia)        */
+        }
+    }
+}
