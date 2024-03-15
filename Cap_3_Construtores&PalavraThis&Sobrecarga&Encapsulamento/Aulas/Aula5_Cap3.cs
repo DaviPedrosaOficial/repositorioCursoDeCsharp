7c805ad8dfbb057d1bf47bc39a01716964b5f07d
DaviPedrosa <pedrosassss@hotmail.com>
2024-03-15 11:11:41 -0300

    Enviando inicio da aula 5

---
+++ Cap_3_Construtores&PalavraThis&Sobrecarga&Encapsulamento/Aulas/Aula5_Cap3.cs
@@ -0,0 +1,22 @@
+﻿/* ------ Encapsulamento ------ */
+
+namespace CursoDeCsharp
+{
+    class Aula5_Cap3
+    {
+        static void _Aula5_Cap3(string[] arg)
+        {
+            /* ------ O que é o encapsulamento? ------
+              O encapsulamento é um pilar muito importante para a orientação à objetos, pois, ele permite nos permite 
+             controlar o que pode e o que não poderá ser feito com nossa classe. 
+             
+              Isso ocorre, de maneira que ao criarmos os atributos e métodos de nossa classe, podemos deifinir se os mesmos
+             serão públicos (outras arquivos terão liberdade de fazerem o que quiserem com o mesmo) ou privado (somente a 
+             classe poderá alterar os mesmos).
+             
+              Sendo assim, o encapsulamento tem como função esconder detalhes de implementações de um componente, expondo 
+             somente operações seguras e que o mantenha em estado consistente. */
+            
+        }
+    }
+}
---
+++ Cap_3_Construtores&PalavraThis&Sobrecarga&Encapsulamento/Classes/Hardware.cs
@@ -0,0 +1,29 @@
+﻿namespace CursoDeCsharp;
+
+public class Hardware
+{
+    /* Definindo que os atributos só poderão ser alterados manualmente na classe, através do pré-fixo "private" */
+    /* A nomenclatura para atributos privados em C# é feita com letra minúscula e um "_" de prefixo, assim como
+     representado abaixo: */
+    private string _tipo;
+    private string _modelo;
+    private string _marca;
+    private double _preco;
+
+    /* Construtores */
+    public Hardware()
+    {
+        _preco = 0.0;
+    }
+
+    public Hardware(string tipo, string modelo) : this()
+    {
+        _tipo = tipo;
+        _modelo = modelo;
+    }
+
+    public Hardware(string tipo, string modelo, string marca) : this(tipo, modelo)
+    {
+        _marca = marca;
+    }
+}
