21dba936a53bc3cc75d2921b3764e72b753ec75f
DaviPedrosa <pedrosassss@hotmail.com>
2024-03-17 15:07:56 -0300

    Enviando aula 5

---
+++ Cap_3_Construtores&PalavraThis&Sobrecarga&Encapsulamento/Aulas/Aula6_Cap3.cs
@@ -0,0 +1,37 @@
+﻿/* ------ Properties (Propriedades) ------ */
+
+namespace CursoDeCsharp
+{
+    class Aula6_Cap3
+    {
+        static void _Aula6_Cap3(string[] arg)
+        {
+            /* ------ O que são properties (propriedades) ------
+             
+             As propriedades são funcionalidades que auxiliam na implementação de métodos a nossas classe. Tornando mais 
+            simples, econômico e otimizado para o nosso código, uma vez que as mesmas tornam desnecessário a implementação 
+            manual das funções get & set.
+            
+             Para criarmos uma propriedade, devemos fazer uma chamada parecida com a de um construtor mas para nossos 
+            atributos, e ao fazermos o mesmo, poderemos chamar os métodos get & set para os mesmos, sem precisarmos 
+            desenvolver 2 funções para nosso projeto. (Veja a execução na classe Lapis)
+            
+             Além disso, a utilização das propriedades fará com que, a chamada de nossos métodos se tornem muito mais 
+            simples como veremos a seguir:                                                                            */
+
+            Lapis lapis = new Lapis("Laranja", "Cis");
+
+            /* Agora, utilizando as propiedades, note como fica mais simples a chamada das funções get & set em nossa
+             main */
+            
+            /* ------ Função set ------ */
+            lapis.Cor = "Azul";
+            lapis.marca = "Faber-Castell";
+
+            /* ------ Função get ------ */
+            Console.WriteLine(lapis.Cor);
+            Console.WriteLine(lapis.marca);
+        }
+    }
+}
+
---
+++ Cap_3_Construtores&PalavraThis&Sobrecarga&Encapsulamento/Classes/Lapis.cs
@@ -0,0 +1,41 @@
+﻿/* ------ Classe referente a aula 6 do Cap. 3 ------ */
+
+using System.Text.Json;
+
+namespace CursoDeCsharp
+{
+    public class Lapis
+    {
+        private string _cor;
+        private string _marca;
+
+        public Lapis(string cor, string marca)
+        {
+            _cor = cor;
+            _marca = marca;
+        }
+
+        /* ------ Implementação das propriedades ------ */
+        public string Cor
+        {
+            get { return _cor; }
+            set {
+                if (value != null && value.Length > 1)
+                {
+                    _cor = value;
+                }}
+        }
+        /* Repare que não foi preciso criar as funções a mão, utilizamos a síntaxe par executar os métodos */
+
+        public string marca
+        {
+            get { return _marca; }
+            set {
+                if (value != null && value.Length > 1)
+                {
+                    _marca = value;
+                }}
+        }
+    }
+}
+
