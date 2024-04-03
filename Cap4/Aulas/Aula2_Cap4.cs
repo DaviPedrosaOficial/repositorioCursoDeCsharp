6db42a81c4e5fc8c3de25287b2a0ba7aa5f3784e
DaviPedrosa <pedrosassss@hotmail.com>
2024-04-03 11:03:01 -0300

    Enviando a aula 1 do Cap 5

--- Cap4/Aulas/Aula2_Cap4.cs
+++ Cap4/Aulas/Aula2_Cap4.cs
@@ -1,63 +1,63 @@
-/* Desalocação de mémoria - Garbage collector e escopo local */
+/* Desalocaï¿½ï¿½o de mï¿½moria - Garbage collector e escopo local */
 
 namespace CursoDeCsharp
 {
     class Aula2_Cap4
     {
-        /* ------ O que é o garbage collector? ------
+        /* ------ O que ï¿½ o garbage collector? ------
            
-          Garbage collector é um processo que automatiza o gerenciamento de mémoria de um programa
-        em execução. 
+          Garbage collector ï¿½ um processo que automatiza o gerenciamento de mï¿½moria de um programa
+        em execuï¿½ï¿½o. 
         
          O garbage collector monitora os objetos alocados dinamicamente pelo programa (no "heap", parte
-        de nossas mémorias), desalocando aqueles que não estão mais sendo utilizados.
+        de nossas mï¿½morias), desalocando aqueles que nï¿½o estï¿½o mais sendo utilizados.
          
-         Ou seja, ao instanciar um objeto, o mesmo contará com uma variável em "stack" (outra parte da 
-        mémoria) que armazenará as coordenadas daquele objeto. 
+         Ou seja, ao instanciar um objeto, o mesmo contarï¿½ com uma variï¿½vel em "stack" (outra parte da 
+        mï¿½moria) que armazenarï¿½ as coordenadas daquele objeto. 
         
-         Porém, é possível que em algum momento em nosso programa esta variável em stack perca tal 
-        coordenada, seja para colocar outra coordenada em sua variável, ou até mesmo para se tornar nula.
+         Porï¿½m, ï¿½ possï¿½vel que em algum momento em nosso programa esta variï¿½vel em stack perca tal 
+        coordenada, seja para colocar outra coordenada em sua variï¿½vel, ou atï¿½ mesmo para se tornar nula.
         
          Veja um exemplo de como um objeto pode ser desalocado:
-        Imagine o seguinte cenário:
+        Imagine o seguinte cenï¿½rio:
 
         Product p1 , p2;
 
-        p1 = new Product("Calça", 250.00 , 0);
+        p1 = new Product("Calï¿½a", 250.00 , 0);
         p2 = new Product("Tenis", 450.00 , 0);
 
-         Até então criamos 2 objetos, e os instanciamos. Mas veja a seguir:
+         Atï¿½ entï¿½o criamos 2 objetos, e os instanciamos. Mas veja a seguir:
 
         p1 = p2;
 
-         Nesse momento, o que está acontecendo é exatamente o que foi citado acima. p1 tinha uma coordenada,
-        de um objeto instanciado, porém o nosso programa solicitou que as coordenadas alocadas em p1 fossem 
+         Nesse momento, o que estï¿½ acontecendo ï¿½ exatamente o que foi citado acima. p1 tinha uma coordenada,
+        de um objeto instanciado, porï¿½m o nosso programa solicitou que as coordenadas alocadas em p1 fossem 
         trocadas pela de p2. O que faz com que o objeto que antes era de p1 se encontra "perdido" em nosssa 
-        mémoria.
+        mï¿½moria.
 
 
 
         
-         Independente do motivo, se nossa instanciação de objeto se manter em heap sem ter outra variável em 
-        stack para aponta-la, o garbage collector desalocará (retirar) essa instanciação, para preservar espaço 
-        em nossa mémorias.
+         Independente do motivo, se nossa instanciaï¿½ï¿½o de objeto se manter em heap sem ter outra variï¿½vel em 
+        stack para aponta-la, o garbage collector desalocarï¿½ (retirar) essa instanciaï¿½ï¿½o, para preservar espaï¿½o 
+        em nossa mï¿½morias.
 
-         Ps.: O garbage collector não fará o mesmo de imediato! Demora alguns processos para que a desalocação 
+         Ps.: O garbage collector nï¿½o farï¿½ o mesmo de imediato! Demora alguns processos para que a desalocaï¿½ï¿½o 
         ocorra.
 
-        ------ Desalocação por escopo ------
+        ------ Desalocaï¿½ï¿½o por escopo ------
 
-         O que é a desalocação por escopo?
+         O que ï¿½ a desalocaï¿½ï¿½o por escopo?
          
-         Desalocação por escopo é o processo onde variáveis são criadas dentro de um escopo, seja esse uma função,
-        método, ... após o encerramento do mesmo, as variáveis que foram alocadas ali, serão imediatamente
+         Desalocaï¿½ï¿½o por escopo ï¿½ o processo onde variï¿½veis sï¿½o criadas dentro de um escopo, seja esse uma funï¿½ï¿½o,
+        mï¿½todo, ... apï¿½s o encerramento do mesmo, as variï¿½veis que foram alocadas ali, serï¿½o imediatamente
         desalocadas.
 
-         Ou seja, quando criamos um loop, ou chamamos um método de uma classe que contém variáveis neles, ao mesmo
-        se encerrar, aqueles que estiverem dentro dos mesmo se encerrarão com eles.
+         Ou seja, quando criamos um loop, ou chamamos um mï¿½todo de uma classe que contï¿½m variï¿½veis neles, ao mesmo
+        se encerrar, aqueles que estiverem dentro dos mesmo se encerrarï¿½o com eles.
 
          Veja o exemplo a seguir do processo:
-         Imagine o seguinte cenário:
+         Imagine o seguinte cenï¿½rio:
 
         double total;
 
@@ -66,14 +66,14 @@ namespace CursoDeCsharp
         total += calcDesc;
         }
 
-         Note que a variável calcDesc existe somente dentro da condicional "if". Sendo assim, o programa quando 
-        chegar a condicional irá alocar a variável calcDesc em stack (mémoria) dentro do escopo do if, e assim que
-        o mesmo se encerrar, a variável sera desalocada também!
-         Obs.: Podemos ter escopos dentro de escopos, onde os mesmos farão o mesmo processo citado acima, assim que 
+         Note que a variï¿½vel calcDesc existe somente dentro da condicional "if". Sendo assim, o programa quando 
+        chegar a condicional irï¿½ alocar a variï¿½vel calcDesc em stack (mï¿½moria) dentro do escopo do if, e assim que
+        o mesmo se encerrar, a variï¿½vel sera desalocada tambï¿½m!
+         Obs.: Podemos ter escopos dentro de escopos, onde os mesmos farï¿½o o mesmo processo citado acima, assim que 
         for encerrado.
 
-         É importante sabermos como o processo aconteça, para que não haja confusão entre garbage collector e
-        desalocação por escopo!
+         ï¿½ importante sabermos como o processo aconteï¿½a, para que nï¿½o haja confusï¿½o entre garbage collector e
+        desalocaï¿½ï¿½o por escopo!
         */
     }
 }
